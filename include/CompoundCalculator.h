d628b60637ee9c1e5a20ac85cf899454cce732e5
AlexKoops <125556152+AlexKoops@users.noreply.github.com>
2025-01-21 15:10:47 +0100

    added compare tool

--- include/CompoundCalculator.h
+++ include/CompoundCalculator.h
@@ -14,9 +14,38 @@ public:
     CompoundCalculator(double principal, double rate, int freq, double time, double contribution, bool useContribution);
 
     void plotInvestment();
+    static void compareScenarios(CompoundCalculator &scenario1, CompoundCalculator &scenario2);
+    void printInfo();
 
     double calculateInterest() const;
     double calculateContribution() const;
+
+    /*------------------------------------------------------------------------------------------------------------------*/
+    
+    // Getters
+    double getPrincipal() const {
+        return principal;
+    }
+
+    double getAnnualRate() const {
+        return annualRate;
+    }
+
+    int getCompoundFreq() const {
+        return compoundFreq;
+    }
+
+    double getYears() const {
+        return years;
+    }
+
+    double getMonthlyContribution() const {
+        return monthlyContribution;
+    }
+
+    bool isUsingMonthlyContribution() const {
+        return useMonthlyContribution;
+    }
 
 };
 
--- src/main.cc
+++ src/main.cc
@@ -11,7 +11,8 @@ void compoundMenu() {
     while(true) {
         cout << "Make a choice based to for functionality:" << endl;
         cout << "- 1. Calculate Interst and display graph" << endl;
-        cout << "- 2. Exit" << endl;
+        cout << "- 2. Compare 2 scenarios" << endl;
+        cout << "- 3. Exit" << endl;
         cout << "> ";
 
         int choice;
@@ -29,12 +30,38 @@ void compoundMenu() {
             double result = calculator.calculateInterest();
             double totalContributions = calculator.calculateContribution();
 
-            cout << endl << "The future value will be: " << fixed << setprecision(2) << result << endl;
-            cout << "Total Contributions: " << fixed << setprecision(2) << totalContributions << endl << endl;
+            calculator.printInfo();
+            cout << endl << "The future balance will be: " << fixed << setprecision(2) << result << endl;
+            cout << "Total principal: " << fixed << setprecision(2) << totalContributions << endl;
+            cout << "Total interest: " << fixed << setprecision(2) << (result - totalContributions) << endl << endl;
 
             calculator.plotInvestment();
         }
         else if(choice == 2) {
+            cout << "You will first be prompted to input the info for scenario 1 then scenario 2." << endl << endl;
+            cout << "Input info for scenario 1: " << endl;
+            double principal1 = InputHandler::getPrincipalAmount();
+            double rate1 = InputHandler::getAnnualInterestRate();
+            int freq1 = InputHandler::getCompFreq();
+            double years1 = InputHandler::getYears();
+            bool useMonthly1 = InputHandler::askForMonthlyContribution();
+            double contribution1 = useMonthly1 ? InputHandler::getMonthlyContribution() : 0.0;
+            CompoundCalculator calculator1(principal1, rate1, freq1, years1, contribution1, useMonthly1);
+
+            cout << endl << "Input info for scenario 2: " << endl;
+            double principal2 = InputHandler::getPrincipalAmount();
+            double rate2 = InputHandler::getAnnualInterestRate();
+            int freq2 = InputHandler::getCompFreq();
+            double years2 = InputHandler::getYears();
+            bool useMonthly2 = InputHandler::askForMonthlyContribution();
+            double contribution2 = useMonthly2 ? InputHandler::getMonthlyContribution() : 0.0;
+
+            CompoundCalculator calculator2(principal2, rate2, freq2, years2, contribution2, useMonthly2);
+
+            CompoundCalculator::compareScenarios(calculator1, calculator2);
+            cout << endl;
+        }
+        else if(choice == 3) {
             break;
         }
     }
